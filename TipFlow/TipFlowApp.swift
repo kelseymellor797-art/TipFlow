e4cba0dd0ffd7d1d8b5ebfc4d417c0e1dcd961a2
Kelsey Mellor <kelseymellor797@gmail.com>
2026-03-08 21:32:59 -0700

    Initial Commit

---
+++ TipFlow/TipFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipFlowApp.swift
+//  TipFlow
+//
+//  Created by Katrina The Ballerina on 3/8/26.
+//
+
+import SwiftUI
+
+@main
+struct TipFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
